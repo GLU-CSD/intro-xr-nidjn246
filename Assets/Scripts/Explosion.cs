aa32fc2381aa90302063963b6b7e91c28eacf8ce
nidjn246 <milanveldhuizen09@gmail.com>
2025-01-09 18:30:53 +0100

    made grenades and donkey

---
+++ Assets/Scripts/Explosion.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Explosion : MonoBehaviour
+{
+    public float explosionForce = 500f;      // Kracht van de explosie
+    public float explosionRadius = 5f;       // Radius van de explosie
+    public float explosionDamage = 20f;      // Damage van de explosie
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy")) // Zorg dat vijanden de tag "Enemy" hebben
+        {
+            Explode();
+            Destroy(gameObject); // Verwijder het object na de explosie
+        }
+    }
+
+    void Explode()
+    {
+        Collider[] colliders = Physics.OverlapSphere(transform.position, explosionRadius);
+        foreach (Collider nearbyObject in colliders)
+        {
+            NavMeshAgent agent = nearbyObject.GetComponent<NavMeshAgent>();
+            if (agent != null)
+            {
+                agent.enabled = false;
+            }
+
+            Health healthScript = nearbyObject.GetComponent<Health>();
+            if (healthScript != null)
+            {
+                healthScript.TakeDamage(explosionDamage);
+            }
+
+            Rigidbody rb = nearbyObject.GetComponent<Rigidbody>();
+            if (rb != null)
+            {
+                rb.AddExplosionForce(explosionForce, transform.position, explosionRadius);
+            }
+        }
+    }
+
+}
+
